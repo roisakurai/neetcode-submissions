4b4dcebe0bc02293fc948f6dce757ec4cbb738b4
Paniroi Sakurai <147335776+roisakurai@users.noreply.github.com>
2026-07-31 19:18:23 +0700

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.go
@@ -0,0 +1,8 @@
+func hasDuplicate(nums []int) bool {
+    seen := make(map[int]struct{})
+    for _, num := range nums {
+        seen[num] = struct{}{}
+    }
+
+    return len(seen) < len(nums)
+}
